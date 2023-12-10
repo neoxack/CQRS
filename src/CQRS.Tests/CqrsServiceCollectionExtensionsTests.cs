1af505e373f5a1f84592f57f02eee22db7dea4f4
Dragunov Semyon <neoxack@gmail.com>
2023-12-10 17:44:02 +0400

    add test

--- src/CQRS.Tests/CqrsServiceCollectionExtensionsTests.cs
+++ src/CQRS.Tests/CqrsServiceCollectionExtensionsTests.cs
@@ -25,4 +25,27 @@ public class CqrsServiceCollectionExtensionsTests
         var pingCommandHandler = serviceProvider.GetService<ICommandHandler<PingCommand, string>>();
         Assert.NotNull(pingCommandHandler);
     }
+    
+    [Fact]
+    // ReSharper disable once InconsistentNaming
+    public void AddCQRS_Scoped()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddCQRS(typeof(PingCommand).Assembly);
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        using var scope = serviceProvider.CreateScope();
+        var scopedServiceProvider = scope.ServiceProvider;
+        var commandDispatcher = scopedServiceProvider.GetService<ICommandDispatcher>();
+        Assert.NotNull(commandDispatcher);
+        
+        var queryDispatcher = scopedServiceProvider.GetService<IQueryDispatcher>();
+        Assert.NotNull(queryDispatcher);
+            
+        var pingQueryHandler = scopedServiceProvider.GetService<IQueryHandler<PingQuery, string>>();
+        Assert.NotNull(pingQueryHandler);
+        
+        var pingCommandHandler = scopedServiceProvider.GetService<ICommandHandler<PingCommand, string>>();
+        Assert.NotNull(pingCommandHandler);
+    }
 }
